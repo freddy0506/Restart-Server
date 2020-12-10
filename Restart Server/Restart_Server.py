d2c108fd4b01605804d25de842267b1535d6a97b
freddy0506 <46132734+freddy0506@users.noreply.github.com>
2020-12-10 22:14:08 +0100

    Update Restart_Server.py

--- Restart Server/Restart_Server.py
+++ Restart Server/Restart_Server.py
@@ -1,15 +1,23 @@
 from mcstatus import MinecraftServer as MineS
 import os
 import time
+from datetime import datetime
+from datetime import date
 
-server = MineS.lookup("Odenwald-Verl.de:25565")
-print("Up")
+server = MineS.lookup("192.168.178.143")
+
+logFile = open("Crash_Log.txt", "a")
+now = datetime.now()
+today = date.today()
+
 
 while(True):
     try:
         ping = server.ping()
-        #print(ping)
+        print(ping)
     except:
+        with open("Crash_Log.txt", "a") as logFile:
+            logFile.write(today.strftime("%D ") + now.strftime("[%H:%M:%S]" + " The Server is (re)starting"))
         print("Starte Server...")
         os.system("java -Xmx5120M -Xms5120M -jar /root/server.jar nogui")
     time.sleep(10)
